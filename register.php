6f0b4c92ca89c4a57da99cd0a494b39b7bc8421b
Satyam Kumar <110112230+Satyam-kaushik@users.noreply.github.com>
2023-04-02 11:30:26 +0530

    Add files via upload

---
+++ register.php
@@ -0,0 +1,59 @@
+<?php
+// Connect to MySQL
+$host = "localhost";
+$username = "satyam";
+$password = "satyam_76";
+$dbname = "dblab8";
+$conn = mysqli_connect($host, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+
+// Process form data
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$first_name = $_POST["first_name"];
+	$last_name = $_POST["last_name"];
+	$email = $_POST["email"];
+	$password = $_POST["password"];
+	$confirm_password = $_POST["confirm_password"];
+
+	// Validate user input
+	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		echo "Invalid email format.";
+		exit();
+	}
+	if (strlen($password) < 8) {
+		echo "Password must be at least 8 characters long.";
+		exit();
+	}
+	if ($password != $confirm_password) {
+		echo "Passwords do not match.";
+		exit();
+	}
+
+	// Insert data into MySQL table
+	$sql = "INSERT INTO users (first_name, last_name, email, password) VALUES ('$first_name', '$last_name', '$email', '$password')";
+	if (mysqli_query($conn, $sql)) {
+		echo "Registration successful.";
+	} else {
+		echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+	}
+}
+
+// Close MySQL connection
+mysqli_close($conn);
+?>
+
+
+<html>
+
+
+<a href="update.html">Update!</a>
+
+
+
+
+</html>
